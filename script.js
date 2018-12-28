22c9ccc3e478f1d25ebd11cc99557657ea545627
Wesley Blokker <w.blokker@gmail.com>
2018-12-28 08:33:38 +0100

    Made game function.
    Game functions, but does not yet keep score.

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-var UserChoice = "rock";
-var CompChoice = compGen();
-
-function compGen () {
+var UserChoice = "rOck";
+var CompChoice = (function() {
   var Options = ["rock", "paper", "scissors"];
   const Throw = Options[Math.floor(Math.random() * 3)];
   return Throw;
+})();
+
+function game(user, computer) {
+  var user = UserChoice.toLowerCase();
+  if (user === CompChoice) {
+    console.log("It's a draw. Try again.");
+  }
+  else {
+    switch (user) {
+      case "rock":
+        if (CompChoice == "scissors") {console.log("You win!")}
+        else {console.log("You lose.")}
+        break;
+      case "scissors":
+        if (CompChoice == "paper") {console.log("You win!")}
+        else {console.log("You lose.")}
+        break;
+      case "paper":
+        if (CompChoice == "rock") {console.log("You win!")}
+        else {console.log("You lose.")}
+        return;
+      default:
+    }
+  }
 }
+game(UserChoice, CompChoice);
