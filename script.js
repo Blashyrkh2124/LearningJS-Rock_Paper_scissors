b3b2a9c0a47016943ece88cc91184fb3e7f0a6fb
Wesley Blokker <w.blokker@gmail.com>
2018-12-28 09:01:04 +0100

    Finished the game in JS
    I have finished the game. It now holds a score for five games and then returns wether you have won or lost in the console.

--- script.js
+++ script.js
@@ -1,31 +1,64 @@
-var UserChoice = "rOck";
-var CompChoice = (function() {
-  var Options = ["rock", "paper", "scissors"];
-  const Throw = Options[Math.floor(Math.random() * 3)];
-  return Throw;
-})();
+let PlayerWins = 0;
+let ComputerWins = 0;
 
-function game(user, computer) {
+function game() {
+  var UserChoice = prompt();
   var user = UserChoice.toLowerCase();
+  var CompChoice = (function() {
+    var Options = ["rock", "paper", "scissors"];
+    const Throw = Options[Math.floor(Math.random() * 3)];
+    return Throw;
+  })();
   if (user === CompChoice) {
-    console.log("It's a draw. Try again.");
+    console.log("Draw...");
   }
   else {
     switch (user) {
       case "rock":
-        if (CompChoice == "scissors") {console.log("You win!")}
-        else {console.log("You lose.")}
+        if (CompChoice == "scissors") {
+          console.log("You win!");
+          ++PlayerWins;
+        }
+        else {
+          console.log("You lose.");
+          ++ComputerWins;
+        }
         break;
       case "scissors":
-        if (CompChoice == "paper") {console.log("You win!")}
-        else {console.log("You lose.")}
+        if (CompChoice == "paper") {
+          console.log("You win!");
+          ++PlayerWins;
+        }
+        else {
+          console.log("You lose.");
+          ++ComputerWins;
+        }
         break;
       case "paper":
-        if (CompChoice == "rock") {console.log("You win!")}
-        else {console.log("You lose.")}
+        if (CompChoice == "rock") {
+          console.log("You win!");
+          ++PlayerWins;
+      }
+        else {
+          console.log("You lose.");
+          ++ComputerWins;
+        }
         return;
-      default:
+      default: (console.log("It appears you made an error."));
     }
   }
 }
-game(UserChoice, CompChoice);
+
+for(i=1; i<=5; i++) {
+  game()
+  console.log("Player wins =" + PlayerWins);
+  console.log("Computer wins= " + ComputerWins);
+  if (i == 5) {
+    if (PlayerWins > ComputerWins) {
+      console.log("The game has ended. You have won.");
+    } else if (PlayerWins < ComputerWins) {
+      console.log("The game has ended. You have lost.");
+    } else (console.log("The game has ended and resulted in a draw."));
+    break;
+  }
+}
