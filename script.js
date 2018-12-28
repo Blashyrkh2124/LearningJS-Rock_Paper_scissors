38a639e5c4b9e8bc4a264bcf9f851e9ba2c774ca
Wesley Blokker <w.blokker@gmail.com>
2018-12-28 07:55:19 +0100

    Simpliedfied copmuter choice. Changed scope of comp choice var.

--- script.js
+++ script.js
@@ -1,25 +1,8 @@
 var UserChoice = "rock";
+var CompChoice = compGen();
 
-function computerChoice() {
+function compGen () {
   var Options = ["rock", "paper", "scissors"];
   const Throw = Options[Math.floor(Math.random() * 3)];
   return Throw;
 }
-
-function rockPaperScissors(player, computer) {
-  var play = UserChoice.toLowerCase();
-  var comp = CompChoice.toLowerCase();
-  if (play === comp) {
-    alert("It's a draw!");
-    return;
-  } else if (play == "rock" && comp == "scissors") {
-    playerWin();
-  } else if (play == "rock") {
-
-  }
-}
-
-function playerWin() {
-  alert("Player wins. Player choice was " + UserChoice + ". Computer choice was " + CompChoice + ".");
-};
-console.log(computerChoice());
